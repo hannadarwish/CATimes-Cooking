dd7ddf02dd61a0c842c7349876d328850d684fcf
hannadarwish <hannadarwish@gmail.com>
2023-06-12 21:41:27 -0700

    auth me backend phase 1 user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+  
+    return nil unless user # Return nil if no user is found
+  
+    if user.authenticate(password)
+      return user # Return the user if the password is correct
+    else
+      return nil # Return nil if the password is incorrect
+    end
+  end
+
+  def reset_session_token!
+    self.update!(session_token: generate_unique_session_token)
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    # in a loop:
+      # use SecureRandom.base64 to generate a random token
+      # use `User.exists?` to check if this `session_token` is already in use
+      # if already in use, continue the loop, generating a new token
+      # if not in use, return the token
+      loop do
+        token = SecureRandom.base64
+        break token unless User.exists?(session_token: token)
+      end
+  end
+
+  def ensure_session_token
+    # if `self.session_token` is already present, leave it be
+    # if `self.session_token` is nil, set it to `generate_unique_session_token`
+    self.session_token ||= generate_unique_session_token
+  end
+
+end  

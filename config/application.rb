766e4181d311237706e42760ae4d67483d5da3e0
hannadarwish <hannadarwish@gmail.com>
2023-06-19 17:52:57 -0700

    deploying to render

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module CftCooking
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.railties_order = [:all, :main_app]
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
